0fa76192094e6ccf1085c3140f3044332e3b0bfd
Tim Browning <tim.browning@renishaw.com>
2010-02-04 11:22:20 +0000

    Updated Build batch file to use local file references.

--- Build exe.bat
+++ Build exe.bat
@@ -1,3 +1,3 @@
-C:\Python25\pyinstaller-1.3\Makespec.py -F --console -o C:\Python25\tim\GoodScripts\MapBOM\Executable\ --icon=mapbom.ico MapBom.py
+C:\Python25\pyinstaller-1.3\Makespec.py -F --console -o Executable\ --icon=mapbom.ico MapBom.py
 
-C:\Python25\pyinstaller-1.3\Build.py C:\Python25\tim\GoodScripts\MapBOM\Executable\MapBom.spec
+C:\Python25\pyinstaller-1.3\Build.py Executable\MapBom.spec
